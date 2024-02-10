7ce87a03fa6b279d9408dabf4eae3bb403504822
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-02-10 21:03:27 +0200

    created the HibernateUtil class to interact with the GenericDao class

---
+++ src/main/java/org/example/hibernate_util/HibernateUtil.java
@@ -0,0 +1,35 @@
+package org.example.hibernate_util;
+
+import lombok.Getter;
+import org.example.entity.Client;
+import org.example.entity.Planet;
+import org.example.entity.Ticket;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+@Getter
+public class HibernateUtil {
+    private static final HibernateUtil INSTANCE;
+
+    static {
+        INSTANCE = new HibernateUtil();
+    }
+
+    private final SessionFactory sessionFactory;
+
+    private HibernateUtil() {
+        sessionFactory = new Configuration()
+                .addAnnotatedClass(Client.class)
+                .addAnnotatedClass(Planet.class)
+                .addAnnotatedClass(Ticket.class)
+                .buildSessionFactory();
+    }
+
+    public static HibernateUtil getInstance() {
+        return INSTANCE;
+    }
+
+    public void close() {
+        sessionFactory.close();
+    }
+}
